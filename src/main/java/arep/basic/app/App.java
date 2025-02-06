35c3b654b43a98428795ba9b53b4997a92f1d488
KenjiroYamato <juanpablocamargo91@gmail.com>
2025-02-06 14:57:02 -0500

    chore: first commit

---
+++ src/main/java/arep/basic/app/App.java
@@ -0,0 +1,13 @@
+package arep.basic.app;
+
+
+public class App {
+
+    public static String message(String a){
+        return "Hola: " + a;
+     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
